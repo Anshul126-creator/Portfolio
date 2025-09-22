a11d1086dd9bbe9861cba2a2ec7da79ad41c0702
Anshul Kumar sen <anshulkumarsen4@gmail.com>
2025-09-22 20:40:26 +0530

    second commit

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -45,7 +45,7 @@ const Hero = () => {
 
 </div>
             <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
-              Hi, I’m Adrian, a developer based in Croatia with a passion for
+              Hi, I’m Anshul, a developer based in india with a passion for
               code.
             </p>
 
